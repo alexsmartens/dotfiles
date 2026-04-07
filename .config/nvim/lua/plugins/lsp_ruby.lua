2508554c35332cfb0f1313c0b0bffacb5fa01066
Alex Martens <alex.martens@coupa.com>
2026-04-06 18:43:32 -0700

    Remove redundant LSP keymaps now provided by Neovim 0.12 defaults

--- .config/nvim/lua/plugins/lsp_ruby.lua
+++ .config/nvim/lua/plugins/lsp_ruby.lua
@@ -24,59 +24,19 @@ return {
 
   keys = {
     {
-      "<leader>d",
+      "<leader>gd",
       function() vim.lsp.buf.definition() end,
       desc = "Go to definition",
     },
     {
-      "<leader>ls",
-      function() vim.lsp.buf.document_symbol() end,
-      desc = "List classes, methods, and constants in file",
-    },
-    {
-      "<leader>lh",
-      function() vim.lsp.buf.hover() end,
-      desc = "Show docs for symbol under cursor",
-    },
-    {
-      "<leader>lr",
-      function() vim.lsp.buf.references() end,
-      desc = "Find all references",
-    },
-    {
-      "<leader>lt",
+      "<leader>gt",
       function() vim.lsp.buf.typehierarchy("supertypes") end,
-      desc = "Show parent classes and included modules",
-    },
-    {
-      "<leader>ln",
-      function() vim.lsp.buf.rename() end,
-      desc = "Rename variable/method/class across files",
-    },
-    {
-      "<leader>la",
-      function() vim.lsp.buf.code_action() end,
-      desc = "Code actions (quickfix, refactor)",
+      desc = "Go to type hierarchy",
     },
     {
       "<leader>lf",
       function() vim.lsp.buf.format() end,
       desc = "Format file",
-    },
-    {
-      "<leader>lw",
-      function() vim.diagnostic.open_float() end,
-      desc = "Show warning/error details",
-    },
-    {
-      "[w",
-      function() vim.diagnostic.goto_prev() end,
-      desc = "Previous warning/error",
-    },
-    {
-      "]w",
-      function() vim.diagnostic.goto_next() end,
-      desc = "Next warning/error",
     },
   }
 }
